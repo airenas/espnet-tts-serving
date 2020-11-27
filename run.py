7d2cf26c95c0b4a7c65c82699b65442e072534ea
Airenas Vaičiūnas <airenass@gmail.com>
2020-11-27 13:50:54 +0200

    Make port configurable

--- run.py
+++ run.py
@@ -15,4 +15,6 @@ logger.info("Starting espnet-tts-serving service")
 app = create_service()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    logger.info("Using port: %d" % port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
