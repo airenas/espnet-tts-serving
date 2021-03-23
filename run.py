8c6cb70d419597222e9567392133c7d561e6c528
Airenas Vaičiūnas <airenass@gmail.com>
2021-03-23 14:58:52 +0200

    Add date to log lines

--- run.py
+++ run.py
@@ -1,15 +1,21 @@
+import logging
 import os
 import sys
 
 import uvicorn
-from service.service import create_service
-
 from fastapi.logger import logger
-import logging
+from uvicorn.config import LOGGING_CONFIG
+
+from service.service import create_service
 
 ll = os.environ.get('LOG_LEVEL', 'INFO').upper()
 logger.setLevel(level=ll)
-logger.handlers = [logging.StreamHandler(sys.stdout)]
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter("[%(asctime)s.%(msecs)03d] %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+LOGGING_CONFIG["formatters"]["default"]["fmt"] = "[%(asctime)s.%(msecs)03d] %(levelname)s - %(message)s"
+LOGGING_CONFIG["formatters"]["default"]["datefmt"] = "%Y-%m-%d %H:%M:%S"
 
 logger.info("Starting espnet-tts-serving service")
 app = create_service()
